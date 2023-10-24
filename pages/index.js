30ff4f77b8c831a8d355250ac31950cbe44f9710
cloverfield11 <130602166+cloverfield11@users.noreply.github.com>
2023-10-24 11:03:55 +0500

    Update index.js
    Translated

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ import { getRandomSeed } from "lib/seeds";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export const appName = "Paint by Text";
-export const appSubtitle = "Edit your photos using written instructions, with the help of an AI.";
-export const appMetaDescription = "Edit your photos using written instructions, with the help of an AI.";
+export const appName = "P2T";
+export const appSubtitle = "Редактируйте изображения прописывая инструкции, при помощи ИИ.";
+export const appMetaDescription = "Редактируйте изображения прописывая инструкции, при помощи ИИ.";
 
 export default function Home() {
   const [events, setEvents] = useState([]);
